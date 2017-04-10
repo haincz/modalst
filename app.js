f56e36bfa432c07e70a2669a5f2e5844ac148cd5
Mikoś Mateusz <haincz@gmail.com>
2017-04-10 08:39:39 +0200

    init

--- app.js
+++ app.js
@@ -1,16 +1,16 @@
-function StekModal () {
-	this.id = "stekmodalmeu";
+function Modal () {
+	this.id = "#stekmodalmeu";
 
-	$(document).on('click', '#stekmodalmeu .closemessanger', function() {
+	$(document).on('click', '#stekmodalmeu .closemodal', function() {
     	$(this.id).fadeOut(800, function() {
       	$(this.id).remove();
     	}.bind(this));
   	}.bind(this));
 }
 
-StekModal.prototype.ModalContent = function(id) {
+Modal.prototype.ModalContent = function(id) {
 	var CONSTANS = {
-		DIV: '<div id="' + StekModal.id + '">',
+		DIV: '<div id="' + Modal.id + '">',
 		DIVEND: '</div>',
 		CONTENT: '<p>Przez 25 lat naszej działalności nastąpiło wiele zmian.</p> <p>I tak począwszy od wizerunku zewnętrznego, nazwy, zakresu i miejsc działalności (Myślenice, Skawina, Zakopane, Kraków…) przez formę i sposób serwowania, skończywszy na samym MENU.</p> <p>Niezmienny pozostał klimat, smak i jakość świadczonych usług…</p> <p>Menu ewoluowało na skutek zmiennych oczekiwań gości oraz panujących różnych trendów w dziedzinie żywienia. W ostatnim czasie wiele się mówi na temat zdrowego jedzenia i pochodzenia produktów spożywczych. W związku z tym pragniemy poinformować Państwa, że pomimo iż wiąże się to ze zwiększonym kosztem ponoszonym przez naszą firmę, podajemy Państwu potrawy przygotowane z najwyższej jakości produktów. Nie używamy powszechnie oferowanych tanich i szkodliwych dla zdrowia półproduktów i dodatków zawierających konserwanty, wypełniacze i inne substancje chemiczne występujące pod dziwnymi nazwami i symbolami…</p> <p>Również w związku z apelem Światowej Organizacji Zdrowia (WHO) używamy w technologii gastronomicznej znacznie mniej tłuszczów i soli oraz węglowodanów do zagęszczania zup i sosów.<p> <p>Mimo wprowadzenia tych zmian udało się nam wypracować kompromis i możemy zaoferować Państwu zdrowe i smaczne jedzenie wykonane zgodnie ze starymi tradycyjnymi recepturami.</p> <p>Mamy nadzieję, że trafimy do Państwa gustów i smaków. Wszelkie uwagi i sugestie prosimy kierować bezpośrednio do właścicieli (pod numer telefonu: 603 123 321).<p> <p>Informujemy również, że oprócz obsługi indywidualnej, organizujemy przyjęcia okolicznościowe (wesela, komunie, grile…) w naszej restauracji, albo w miejscu wskazanym przez Państwa – catering (więcej informacji pod numerem telefonu: 603 123 321).</p> <p>Pozdrawiamy i życzymy „smacznego”<p> <p>Joanna i Jacek Szostak</p>'
 	}
@@ -18,7 +18,7 @@ StekModal.prototype.ModalContent = function(id) {
 	$('body').append(CONSTANS.DIV + CONSTANS.CONTENT + CONSTANS.DIVEND);
 };
 
-var stekModal = new StekModal();
+var stekModal = new Modal();
 
 $(document).ready(function() {
 
